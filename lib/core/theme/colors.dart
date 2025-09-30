e38073d1ef65fda180fb08ad333edbbfc7e85565
seoiiwon <wldnjstj99@gmail.com>
2025-09-30 20:12:45 +0900

    ✨ Feature: "static file 추가"
    font, icon 추가
    color, string 작성
    
    related to: #1

---
+++ lib/core/theme/colors.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+
+abstract final class AppColors {
+  static const backgroundBlack = Color(0xFF0A090E);
+  static const boxGray = Color(0xFF1A191E);
+  static const popupGray = Color(0xFF2B2A2F);
+
+  static const gray80 = Color(0xFF3F3F3F);
+  static const gray60 = Color(0xFF676767);
+  static const gray40 = Color(0xFFB5B5B5);
+  static const gray20 = Color(0xFFE8E8E8);
+  static const white = Color(0xFFFFFFFF);
+
+  static const primaryBlue = Color(0xFF0098D9);
+  static const orange = Color(0xFFFFB400);
+  static const pinkRed = Color(0xFFFF4D6D);
+  static const popupDeleteRed = Color(0xFFF03A30);
+}
---
+++ lib/core/theme/strings.dart
@@ -0,0 +1,46 @@
+abstract final class AppStrings {
+  static const title = "GREW";
+
+  // HOME
+  static const grewEntryPointTitle = "GREW 둘러보기";
+  static const grewEntryPointSubTitle = "함께 하고싶은 GREW를 찾아봐요.";
+
+  static const unauthenticatedUserNotice = "회원 전용 컨텐츠입니다.";
+  static const unauthenticatedUserButton = "가입하기";
+
+  // GREW List
+  // filter
+  static const filterWishlist = "즐겨찾기";
+  static const filterType = "유형";
+
+  // sort (추가 필요)
+  static const sortLatestActivities = "최근 활동 순";
+  static const sortFirstName = "이름 순";
+
+  // Recruitment Status
+  static const requiring = "모집 중";
+  static const endOfRecruitment = "모집 종료";
+  static const nearEndOfRecruitment = "모집 종료 D-";
+
+  // GREW Info
+  static const grewInfoTitle = "GREW 정보";
+  static const verifiedGrew = "경상국립대학교 중앙동아리";
+  static const grewActivities = "활동 보러가기";
+  static const contactGrew = "문의하기";
+
+  static const grewActivityTitle = "GREW 활동";
+
+  // MY
+  static const myTitle = "내 정보";
+  static const verifiedUser = "경상국립대학교 인증 회원";
+  static const updateProfile = "프로필 편집";
+  static const affiliatedGrew = "소속 GREW";
+
+  static const requireLogin = "로그인을 해주세요.";
+  static const welcome = "환영합니다";
+
+  // Onboarding
+  static const onboardingTitle = "회원가입";
+  static const onboardingNoticeTitle = "GREW 이용을 위해 사용자 정보를 입력해주세요.";
+  static const onboardingNoticeSubTitle = "사용자 정보는 안전하게 보관되며 외부에 공유되지 않습니다.";
+}
